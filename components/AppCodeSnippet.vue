b3312caa8acbe31ef006b219cf1dd84437004b85
sg1asgard <sg1asgard@outlook.com>
2020-04-21 16:30:07 +0300

    Copy component / wip

--- components/AppCodeSnippet.vue
+++ components/AppCodeSnippet.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <div class="app-codesnippet">
-      <b-button size="is-small is-primary copy-code" :click="copyCode">
+      <b-button size="is-small is-primary copy-code" @click="copyCode()">
         Copy
       </b-button>
-      <pre><code><slot /></code></pre>
+      <pre><code class="code-to-copy"><slot /></code></pre>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
+
 // TODO: make copy button work
-export default {
+export default Vue.extend ({
   name: 'AppCodeSnippet',
   props: {
     codesnippet: {
@@ -24,16 +26,19 @@ export default {
     }
   },
   computed: {
-    justConsole () {
-      return 'the fuck';
-    }
   },
   methods: {
     copyCode () {
-      return 'button clicked'
+      const copyCode: any = document.querySelector('.copy-code');
+      const copiedCode: any = copyCode.closest('.code-to-copy');
+
+      copiedCode.focus();
+      copiedCode.select();
+
+      console.log (copiedCode);
     }
   }
-}
+})
 </script>
 
 <style lang="scss">
