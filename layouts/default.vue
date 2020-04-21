dbe76bdb3ecd99138c6a7c02a26796b88b15453b
sg1asgard <sg1asgard@outlook.com>
2020-04-21 11:04:24 +0300

    Adding vue-shims.d.ts to fix TS lins errors for importing components with no.vue extension

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,59 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/open-view-white.svg"
-            alt="OpenVue"
-            height="28"
+    <div class="container">
+      <nav
+        class="navbar header has-shadow is-primary"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="navbar-brand">
+          <a
+            class="navbar-item"
+            href="/"
           >
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns is-mobile">
-      <aside class="column is-4 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
+            <img
+              src="~assets/open-view-white.svg"
+              alt="OpenVue"
+              height="28"
             >
-              <!-- <b-icon :icon="item.icon" /> {{ item.title }} -->
-              {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
+          </a>
 
-      <div class="container column is-8">
-        <nuxt />
-      </div>
-    </section>
+          <div class="navbar-burger">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+      </nav>
+    </div>
+    
+    <div class="container">
+      <section class="main-content columns is-mobile">
+        <aside class="column is-4 section">
+          <p class="menu-label is-hidden-touch">
+            General
+          </p>
+          <ul class="menu-list">
+            <li
+              v-for="(item, key) of items"
+              :key="key"
+            >
+              <nuxt-link
+                :to="item.to"
+                exact-active-class="is-active"
+              >
+                <!-- <b-icon :icon="item.icon" /> {{ item.title }} -->
+                {{ item.title }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </aside>
+
+        <div class="container column is-8">
+          <nuxt />
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
